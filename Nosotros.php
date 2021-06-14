d3c8af462a41446d3f9eb97e4f36cfa09786a58b
MigAnglML <65926393+MigAnglML@users.noreply.github.com>
2021-06-14 14:47:50 -0500

    z

---
+++ Nosotros.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js"> <!--<![endif]-->
+	<link rel="shortcut icon" href="./Recursos/Logos/R oficial.png" rel="icon"/>
+    <head>
+    	<!-- meta character set -->
+        <meta charset="utf-8">
+		<!-- Always force latest IE rendering engine or request Chrome Frame -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Rush Tecnologias</title>		
+		<!-- Meta Description -->
+        <meta name="description" content="Blue One Page Creative HTML5 Template">
+        <meta name="keywords" content="one page, single page, onepage, responsive, parallax, creative, business, html5, css3, css3 animation">
+        <meta name="author" content="Muhammad Morshed">
+		
+		<!-- Mobile Specific Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- CSS
+		================================================== -->
+		
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+		
+		<!-- Fontawesome Icon font -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/slit-slider.css">
+		<!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/animate.css">
+		<!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/main.css">
+
+		<!-- Modernizer Script for old Browsers -->
+        <script src="js/modernizr-2.6.2.min.js"></script>
+
+    </head>
+	
+    <body id="body">
+
+		<!-- preloader -->
+		<div id="preloader">
+            <div class="loder-box">
+            	<div class="battery"></div>
+            </div>
+		</div>
+		<!-- end preloader -->
+
+        <!--
+        Fixed Navigation
+        ==================================== -->
+        <?php include_once('comun/menu.php') ?>
+		
+        <!--
+        End Fixed Navigation
+        ==================================== -->
+		
+		<main class="site-content" role="main">
+		
+        <!--
+        Home Slider
+        ==================================== -->
+		
+		
+        <!--
+        End Home SliderEnd
+        ==================================== -->
+			
+	
+			<!-- end Price section -->
+			
+			<!-- Social section -->
+			<section id="social" class="parallax">
+				<div class="overlay">
+					<div class="container">
+						<div class="row">
+						
+							<div class="sec-title text-center white wow animated fadeInDown">
+								<h2>¿Quienes somos?</h2>
+								<p>Somos una empresa</p>
+							</div>
+							
+							<ul class="social-button">
+								<li class="wow animated zoomIn"><a href="#"><i class="fa fa-thumbs-up fa-2x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-2x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-dribbble fa-2x"></i></a></li>							
+							</ul>
+							
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- end Social section -->
+			<div class="col-sm-7 mx-auto">
+					<header class="section-header text-center">
+					<span class="h1 d-block">
+						<span>❝</span>
+					</span>
+					<h2>Happy Customers</h2>
+					</header>
+				</div>
+				
+				<div id="flex-container" class="testimonials">
+					<div id="left-zone">
+					<ul class="list">
+						<li class="item">
+						<input type="radio" id="radio_testimonial-1" name="basic_carousel" checked="checked" />
+						<label class="label_testimonial-1" for="radio_testimonial-1">Diamond Pest Elimination</label>
+						<div class="content-test content_testimonial-1">
+							<span class="picto"></span>
+							<h1>Diamond Pest Elimination</h1>
+							<p>“The team really takes pride in their work. If I didn’t know any better I would think they actually worked for my company.”</p>
+							<p class="testimonialFrom">Bill, Owner</p>
+							<p class="testimonialState">Rochester, NY</p>
+						</div>
+						</li>
+						<li class="item">
+						<input type="radio" id="radio_testimonial-2" name="basic_carousel" />
+						<label class="label_testimonial-2" for="radio_testimonial-2">A+ Handyman Service</label>
+						<div class="content-test content_testimonial-2">
+							<span class="picto"></span>
+							<h1>A+ Handyman Service</h1>
+							<p>“Quite simply… the service offers prompt response time to my visitors and helps me to better know what type of project a potential customer wants.”</p>
+							<p class="testimonialFrom">Bill, Owner</p>
+							<p class="testimonialState">Tucson, AZ</p>
+							<br>
+						</div>
+						</li>
+						<li class="item">
+						<input type="radio" id="radio_testimonial-3" name="basic_carousel" />
+						<label class="label_testimonial-3" for="radio_testimonial-3">Mod Movers</label>
+						<div class="content-test content_testimonial-3">
+							<span class="picto"></span>
+							<h1>Mod Movers</h1>
+							<p>“I couldn’t believe it. I actually had to hire someone to help me keep up with the new business. I had no idea my website had so much value.”</p>
+							<p class="testimonialFrom">Marlene, Owner</p>
+							<p class="testimonialState">Monterey, CA</p>
+						</div>
+						</li>
+						<li class="item">
+						<input type="radio" id="radio_testimonial-4" name="basic_carousel" />
+						<label class="label_testimonial-4" for="radio_testimonial-4">AK Pest Control</label>
+						<div class="content-test content_testimonial-4">
+							<span class="picto"></span>
+							<h1>AK Pest Control</h1>
+							<p>Great company to send leads. Very efficient and pleased with the services. We get lots of leads and that whats important. Support is also great from the managers/support. Thanks YPC Chat</p>
+							<p class="testimonialFrom">Mark, Owner</p>
+							<p class="testimonialState">Somerset, VA</p>
+							<br>
+						</div>
+						</li>
+					</ul>
+					</div>
+					<div id="right-zone"></div>
+				</div>
+
+
+			
+			<!-- Contact section -->
+			<section id="contact" >
+				<div class="container">
+					<div class="row">
+						
+						<div class="sec-title text-center wow animated fadeInDown">
+							<h2>Contact</h2>
+							<p>Leave a Message</p>
+						</div>
+						
+						
+						<div class="col-md-7 contact-form wow animated fadeInLeft">
+							<form action="#" method="post">
+								<div class="input-field">
+									<input type="text" name="name" class="form-control" placeholder="Your Name...">
+								</div>
+								<div class="input-field">
+									<input type="email" name="email" class="form-control" placeholder="Your Email...">
+								</div>
+								<div class="input-field">
+									<input type="text" name="subject" class="form-control" placeholder="Subject...">
+								</div>
+								<div class="input-field">
+									<textarea name="message" class="form-control" placeholder="Messages..."></textarea>
+								</div>
+						       	<button type="submit" id="submit" class="btn btn-blue btn-effect">Send</button>
+							</form>
+						</div>
+						
+						<div class="col-md-5 wow animated fadeInRight">
+							<address class="contact-details">
+								<h3>Contact Us</h3>						
+								<p><i class="fa fa-pencil"></i>Phoenix Inc.<span>PO Box 345678</span> <span>Little Lonsdale St, Melbourne </span><span>Australia</span></p><br>
+								<p><i class="fa fa-phone"></i>Phone: (415) 124-5678 </p>
+								<p><i class="fa fa-envelope"></i>website@yourname.com</p>
+							</address>
+						</div>
+			
+					</div>
+				</div>
+			</section>
+			<!-- end Contact section -->
+			
+			
+		
+		</main>
+		
+		<footer id="footer">
+			<div class="container">
+				<div class="row text-center">
+					<div class="footer-content">
+						<div class="wow animated fadeInDown">
+							<p>newsletter signup</p>
+							<p>Get Cool Stuff! We hate spam!</p>
+						</div>
+						<form action="#" method="post" class="subscribe-form wow animated fadeInUp">
+							<div class="input-field">
+								<input type="email" class="subscribe form-control" placeholder="Enter Your Email...">
+								<button type="submit" class="submit-icon">
+									<i class="fa fa-paper-plane fa-lg"></i>
+								</button>
+							</div>
+						</form>
+						<div class="footer-social">
+							<ul>
+								<li class="wow animated zoomIn"><a href="#"><i class="fa fa-thumbs-up fa-3x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-skype fa-3x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-dribbble fa-3x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="#"><i class="fa fa-youtube fa-3x"></i></a></li>
+							</ul>
+						</div>
+						
+						<p>Copyright &copy; 2014-2015 Design and Developed By<a href="http://www.themefisher.com">Themefisher</a> </p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+		<!-- Essential jQuery Plugins
+		================================================== -->
+		<!-- Main jQuery -->
+        <script src="js/jquery-1.11.1.min.js"></script>
+		<!-- Twitter Bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- Single Page Nav -->
+        <script src="js/jquery.singlePageNav.min.js"></script>
+		<!-- jquery.fancybox.pack -->
+        <script src="js/jquery.fancybox.pack.js"></script>
+		<!-- Google Map API -->
+		<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<!-- Owl Carousel -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- jquery easing -->
+        <script src="js/jquery.easing.min.js"></script>
+        <!-- Fullscreen slider -->
+        <script src="js/jquery.slitslider.js"></script>
+        <script src="js/jquery.ba-cond.min.js"></script>
+		<!-- onscroll animation -->
+        <script src="js/wow.min.js"></script>
+		<!-- Custom Functions -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -53,40 +53,9 @@
         <!--
         Fixed Navigation
         ==================================== -->
-        <header id="navigation" class="navbar-inverse navbar-fixed-top animated-header">
-            <div class="container">
-                <div class="navbar-header">
-                    <!-- responsive nav button -->
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-                    </button>
-					<!-- /responsive nav button -->
-					
-					<!-- logo -->
-					<h1 class="navbar-brand">
-						<a href="#body">Blue</a>
-					</h1>
-					<!-- /logo -->
-                </div>
+        
 
-				<!-- main nav -->
-                <nav class="collapse navbar-collapse navbar-right" role="navigation">
-                    <ul id="nav" class="nav navbar-nav">
-                        <li><a href="#body">Home</a></li>
-                        <li><a href="#service">Service</a></li>
-                        <li><a href="#portfolio">portfolio</a></li>
-                        <li><a href="#testimonials">Testimonial</a></li>
-                        <li><a href="#price">price</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </nav>
-				<!-- /main nav -->
-				
-            </div>
-        </header>
+		
         <!--
         End Fixed Navigation
         ==================================== -->
