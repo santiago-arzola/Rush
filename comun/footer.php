92eead04eaf6e55c2007b7d944780550017de06c
MiguelMurilloLan <78433671+MiguelMurilloLan@users.noreply.github.com>
2021-06-21 17:04:05 -0500

    Update footer.php

--- comun/footer.php
+++ comun/footer.php
@@ -1,30 +1,57 @@
 <footer id="footer">
-			<div class="container">
-				<div class="row text-center">
-					<div class="footer-content">
-						<div class="wow animated fadeInDown">
-							<p>newsletter signup</p>
-							<p>Get Cool Stuff! We hate spam!</p>
-						</div>
-						<form action="#" method="post" class="subscribe-form wow animated fadeInUp">
-							<div class="input-field">
-								<input type="email" class="subscribe form-control" placeholder="Enter Your Email...">
-								<button type="submit" class="submit-icon">
-									<i class="fa fa-paper-plane fa-lg"></i>
-								</button>
-							</div>
-						</form>
-						<div class="footer-social">
+			        <div>
+            <div>
+
+                <div>
+                    <div>
+                        <div class="col-md-4">
+                            <div>
+                                <h2>Links adicionales</h2>
+                            </div>
+                             <ul >
+                                <li><a href="acercade.html"><i class="fa fa-angle-right"></i> Acerca de nosotros</a>
+                                </li>
+                                <li><a href="politica.html"><i class="fa fa-angle-right"></i> Politica de
+                                        privacidad</a></li>
+                              <!--  <li><a href="blog.html"><i class="fa fa-angle-right"></i> Blog</a></li>-->
+                                <li><a href="contacto.html"><i class="fa fa-angle-right"></i> Contactanos</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="main-heading left_text">
+                                <h2>Servicios</h2>
+                            </div>
+                            <ul>
+                                <li><a href="intelligencia.html"><i class="fa fa-angle-right"></i> Inteligencia de
+                                        negocios</a></li>
+                                <li><a href="infraestructura.html"><i class="fa fa-angle-right"></i>
+                                        Infraestructura</a></li>
+                                <li><a href="logistica.html"><i class="fa fa-angle-right"></i> Logistica</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <div>
+                                <h2>Contactanos</h2>
+                            </div>
+                            <p>Lomas de San Francisco no. 8<br>
+                                Col. Lomas de Cervera, Guanajuato, Gto.<br>
+                                <span><a href="tel:+9876543210">473 6903832</a></span></p>
+                           <div class="footer-social">
 							<ul>
-								<li class="wow animated zoomIn"><a href="#"><i class="fa fa-thumbs-up fa-3x"></i></a></li>
 								<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
-								<li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-skype fa-3x"></i></a></li>
-								
+								<li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-instagram fa-3x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="#"><i class="fa fa-facebook fa-3x"></i></a></li>
 							</ul>
 						</div>
-						
-						<p>Copyright &copy; 2014-2015 Design and Developed By<a href="http://www.themefisher.com">Themefisher</a> </p>
-					</div>
-				</div>
-			</div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p>Rushtec © Copyrights 2021</p>
+                </div>
+            </div>
+        </div>
 		</footer>
