4938935a3a986de33f685ac609f6953d247daa80
MiguelMurilloLan <78433671+MiguelMurilloLan@users.noreply.github.com>
2021-06-22 13:27:26 -0500

    footer

--- comun/footer.php
+++ comun/footer.php
@@ -1,37 +1,37 @@
 <style>
-	
+	li{list-style: none;}
 	p{color: gray}
 	a{color: gray;}
 	a:hover{color: #039ee3;}
 </style>
 <footer id="footer">
-			        <div>
+		 <div>
             <div>
 
                 <div>
-                    <div>
+                    <div style="margin: 2rem;">
                         <div class="col-md-4">
                             <div>
-                                <h4 style="color:white; text-transform:uppercase; letter-spacing: 2px; margin-left: 1rem">Links adicionales</h4>
+                                <h4 style="color:white; text-transform:uppercase;letter-spacing: 2px;">Links adicionales</h4>
                             </div>
-                             <ul style="margin-left: -1rem">
-                                <li><a href="acercade.html" ><i class="fa fa-angle-right"></i> Acerca de nosotros</a>
+                             <ul>
+                                <li><a href="./Nosotros.php" ><i class="fa fa-angle-right"></i> Acerca de nosotros</a>
                                 </li>
-                                <li><a href="politica.html"><i class="fa fa-angle-right"></i> Política de
+                                <!--<li><a href="politica.html"><i class="fa fa-angle-right"></i> Política de
                                         privacidad</a></li>
-                              <!--  <li><a href="blog.html"><i class="fa fa-angle-right"></i> Blog</a></li>-->
-                                <li><a href="contacto.html"><i class="fa fa-angle-right"></i> Contáctanos</a></li>
+                              <li><a href="blog.html"><i class="fa fa-angle-right"></i> Blog</a></li>-->
+                                <li><a href="./Contacto.php"><i class="fa fa-angle-right"></i> Contáctanos</a></li>
                             </ul>
                         </div>
                         <div class="col-md-4">
                             <div class="main-heading left_text">
                                 <h4 style="color:white; text-transform:uppercase; letter-spacing: 2px">Servicios</h4>
                             </div>
-                            <ul style="margin-left: -2rem">
-                                <li><a href="intelligencia.html"><i class="fa fa-angle-right"></i> Inteligencia de negocios</a></li>
-                                <li><a href="infraestructura.html"><i class="fa fa-angle-right"></i>
+                            <ul>
+                                <li><a href="./IN.php"><i class="fa fa-angle-right"></i> Inteligencia de negocios</a></li>
+                                <li><a href="./Infraestructura.php"><i class="fa fa-angle-right"></i>
                                         Infraestructura</a></li>
-                                <li><a href="logistica.html"><i class="fa fa-angle-right"></i> Logistica</a>
+                                <li><a href="./Logistica.php"><i class="fa fa-angle-right"></i> Logistica</a>
                                 </li>
                             </ul>
                         </div>
@@ -42,12 +42,12 @@
                             <p >Lomas de San Francisco no. 8<br>
                                 Col. Lomas de Cervera, Guanajuato, Gto.<br>
                                 <span><a href="tel:+9876543210" >473 6903832</a></span></p>
-                           <div class="footer-social">
-							<ul>
-								<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
-								<li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-instagram fa-3x"></i></a></li>
-								<li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="#"><i class="fa fa-facebook fa-3x"></i></a></li>
-							</ul>
+                           <div class="footer-social" style="margin-top:3rem" >
+								<ul >
+									<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
+									<li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-instagram fa-3x"></i></a></li>
+									<li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="#"><i class="fa fa-facebook fa-3x"></i></a></li>
+								</ul>
 						</div>
 
 
@@ -55,8 +55,8 @@
                     </div>
                 </div>
                 <div>
-                    <p>Rushtec © Copyrights 2021</p>
+                    <p style="text-align: center;">Rushtec © Copyrights 2021</p>
                 </div>
             </div>
         </div>
-		</footer>
+</footer>
